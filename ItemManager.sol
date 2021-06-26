fbd33da849bfca08174cbbed229dd71348f24ca5
Abhigya Ghimire <abhigya95@users.noreply.github.com>
2021-06-26 04:14:52 -0700

    Added item contract for each item created

--- ItemManager.sol
+++ ItemManager.sol
@@ -1,10 +1,12 @@
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.6;
+import "./Item.sol";
 
 contract ItemManager{
     
     enum SupplyChainState {Created, Paid, Delivered}
     
     struct S_Item {
+        Item _item;
         string _identifier;
         uint _itemPrice;
         ItemManager.SupplyChainState _state;
@@ -13,13 +15,15 @@ contract ItemManager{
     mapping(uint=> S_Item) public items;
     uint itemIndex;
     
-    event SupplyChainStep(uint _itemIndex, uint _step);
+    event SupplyChainStep(uint _itemIndex, uint _step, address _itemAddress);
     
     function createItem(string memory _identifier, uint _itemPrice) public{
+        Item item = new Item(this, _itemPrice, itemIndex);
+        items[itemIndex]._item = item;
         items[itemIndex]._identifier = _identifier;
         items[itemIndex]._itemPrice = _itemPrice;
         items[itemIndex]._state = SupplyChainState.Created;
-        emit SupplyChainStep(itemIndex, uint(items[itemIndex]._state));
+        emit SupplyChainStep(itemIndex, uint(items[itemIndex]._state), address(item));
         itemIndex++;
     }
     
@@ -28,13 +32,13 @@ contract ItemManager{
         require(items[_itemIndex]._state == SupplyChainState.Created, "Item is further in the chain" );
         
         items[_itemIndex]._state = SupplyChainState.Paid;
-        emit SupplyChainStep(_itemIndex, uint(items[_itemIndex]._state));
+        emit SupplyChainStep(_itemIndex, uint(items[_itemIndex]._state), address(items[_itemIndex]._item));
     }
     
     function triggerDelivery(uint _itemIndex) public{
         require(items[_itemIndex]._state == SupplyChainState.Paid, "Item is further in the chain" );
         
         items[_itemIndex]._state = SupplyChainState.Delivered;
-        emit SupplyChainStep(_itemIndex, uint(items[_itemIndex]._state));
+        emit SupplyChainStep(_itemIndex, uint(items[_itemIndex]._state), address(items[_itemIndex]._item));
     }
 }
